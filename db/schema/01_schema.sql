08fc1cebd0fba2e42dac62b058f3325c58eda5b2
William Ye <w3ye@pm.me>
2021-08-21 19:40:44 +0000

    added email row to users

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -6,7 +6,8 @@ DROP TABLE IF EXISTS user_votes CASCADE;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    last_name VARCHAR(255) NOT NULL,
+    email TEXT
 );
 
 CREATE TABLE events(
