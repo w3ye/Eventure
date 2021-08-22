52c9b2cb43223bf10cf537fd80de09c4e5d89587
Dianne <mdianne.gabriel@gmail.com>
2021-08-22 17:54:08 -0400

    Modify daterangepicker

--- public/scripts/date-range.js
+++ public/scripts/date-range.js
@@ -1,35 +1,41 @@
-$(function() {
-  $('.datefilter').dateRangePicker({
-    // autoUpdateInput: false,
-    // locale: {
-    //   cancelLabel: 'Clear'
-    // },
-    // "minDate": "-1d",
-    // minDate: today,
-    // minDate: Date.now(),
-    // locale: {
-    //   format: 'YYYY/MM/DD'
-    // },
-    // timePicker: true,
-    // timePickerIncrement: 5,
-    // timePicker12Hour: true,
-    // timePickerSeconds: false,
-    // minDate: 0,
-      autoclose: true,
-      todayHighlight: true,
-      format: "mm-dd-yyyy",
-      minDate: 0,
-      clearBtn: true,
-      multidate: false,
-      multidateSeparator: ",",
-      toggleActive: true,
-      startDate: '08-10-2021',
-      endDate: '09-10-2021'
-  });
-  $('.datefilter').on('apply.daterangepicker', function(ev, picker) {
-    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
-  });
-  $('.datefilter').on('cancel.daterangepicker', function(ev, picker) {
-    $(this).val('');
-  });
+// $(function() {
+//   $('.datefilter').dateRangePicker({
+//     // autoUpdateInput: false,
+//     // locale: {
+//     //   cancelLabel: 'Clear'
+//     // },
+//     // "minDate": "-1d",
+//     // minDate: today,
+//     // minDate: Date.now(),
+//     // locale: {
+//     //   format: 'YYYY/MM/DD'
+//     // },
+//     // timePicker: true,
+//     // timePickerIncrement: 5,
+//     // timePicker12Hour: true,
+//     // timePickerSeconds: false,
+//     // minDate: 0,
+//       autoclose: true,
+//       todayHighlight: true,
+//       format: "mm-dd-yyyy",
+//       minDate: 0,
+//       clearBtn: true,
+//       multidate: false,
+//       multidateSeparator: ",",
+//       toggleActive: true,
+//       startDate: '08-10-2021',
+//       endDate: '09-10-2021'
+//   });
+//   $('.datefilter').on('apply.daterangepicker', function(ev, picker) {
+//     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+//   });
+//   $('.datefilter').on('cancel.daterangepicker', function(ev, picker) {
+//     $(this).val('');
+//   });
+// });
+
+$(document).ready(function() {
+  $('.datefilter').dateRangePicker()
+
 });
+
