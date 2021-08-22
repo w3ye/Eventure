908da54ad5ff5e4f344db548d65918a97f0bd0ad
Dianne <mdianne.gabriel@gmail.com>
2021-08-22 19:50:04 -0400

    Update

--- public/scripts/date-range.js
+++ public/scripts/date-range.js
@@ -1,68 +1,56 @@
-// $(function() {
-//   $('.datefilter').dateRangePicker({
-//     // autoUpdateInput: false,
-//     // locale: {
-//     //   cancelLabel: 'Clear'
-//     // },
-//     // "minDate": "-1d",
-//     // minDate: today,
-//     // minDate: Date.now(),
-//     // locale: {
-//     //   format: 'YYYY/MM/DD'
-//     // },
-//     // timePicker: true,
-//     // timePickerIncrement: 5,
-//     // timePicker12Hour: true,
-//     // timePickerSeconds: false,
-//     // minDate: 0,
-//       autoclose: true,
-//       todayHighlight: true,
-//       format: "mm-dd-yyyy",
-//       minDate: 0,
-//       clearBtn: true,
-//       multidate: false,
-//       multidateSeparator: ",",
-//       toggleActive: true,
-//       startDate: '08-10-2021',
-//       endDate: '09-10-2021'
-//   });
-//   $('.datefilter').on('apply.daterangepicker', function(ev, picker) {
-//     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
-//   });
-//   $('.datefilter').on('cancel.daterangepicker', function(ev, picker) {
-//     $(this).val('');
-//   });
-// });
-
 $(document).ready(function () {
   const dateFormat = "mm/dd/yy",
-    from = $( "#from" )
+    from = $("#from")
       .datepicker({
         defaultDate: "+1w",
         changeMonth: true,
         numberOfMonths: 2,
-        minDate: 0
+        minDate: 0,
       })
-      .on( "change", function() {
-        to.datepicker( "option", "minDate", getDate( this ) );
+      .on("change", function() {
+        to.datepicker("option", "minDate", getDate(this));
       }),
-    to = $( "#to" ).datepicker({
+    to = $("#to").datepicker({
       defaultDate: "+1w",
       changeMonth: true,
-      numberOfMonths: 2
+      numberOfMonths: 2,
     })
-    .on( "change", function() {
-      from.datepicker( "option", "maxDate", getDate( this ) );
+    .on("change", function() {
+      from.datepicker("option", "maxDate", getDate(this));
     });
 
-  function getDate( element ) {
-    var date;
+  const getDate = function(element) {
+    let date;
     try {
-      date = $.datepicker.parseDate( dateFormat, element.value );
-    } catch( error ) {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch(error) {
       date = null;
     }
-
+    console.log(date)
     return date;
   }
+
+
+  $("#deadline")
+    .datepicker({
+      minDate: 0,
+      maxDate: "+1M +10D",
+      startDate: Date.now(),
+    });
 });
+
+// $(function() {
+//   $("#datepicker").datepicker({
+//     minDate: 0,
+//     dateFormat: "yy-mm-dd",
+//     onSelect: function(date, datepicker) {
+//       let dependent= $(this).data('dependent');
+//       let thisDate = $(this).val();
+//       $(dependent).datepicker("option", "minDate", thisDate);
+//       $(dependent).datepicker('refresh');
+//     }
+//   });
+//   $("#datepicker2").datepicker({
+//     dateFormat: "yy-mm-dd"
+//   });
+// });
---
+++ public/scripts/deadline.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#datepicker").datepicker({ minDate: 0, maxDate: "+1M +10D" });
+} );
