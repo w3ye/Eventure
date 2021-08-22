eaabe095c5174bc38a0bf401bbe4a9e8c7f06cb3
Dianne <mdianne.gabriel@gmail.com>
2021-08-21 20:25:26 -0400

    Update formatting and style

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,10 +5,17 @@ const escape = function(str) {
 };
 
 $(document).ready(function() {
-  $('.master-body').hide();
-  $('.title').on('click', () => {
-    $('.master-body').slideDown();
-    $('.master-body').show();
-    $('.master-header').css('border-bottom', '1px solid #9B97A8')
+  // $('.master-body').hide();
+  // $('.title').on('click', () => {
+  //   $('.master-body').slideDown();
+  //   $('.master-body').show();
+  //   // $('.master-header').css('border-bottom', '1px solid #9B97A8')
+  //   $('.master-header').css('box-shadow', '0 50px 200px -200px rgba(0,0,0,0.5), 0 10px 10px -10px rgba(0,0,0,0.3)')
+  // });
+
+  $('.submission').on('click', () => {
+    $('.catchphrase').hide();
   });
+
 });
+
