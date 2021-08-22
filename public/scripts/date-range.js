6f74f60e70704b6b7a44826b88a1854c36e38bcd
Dianne <mdianne.gabriel@gmail.com>
2021-08-22 17:24:01 -0400

    Modify minDate

--- public/scripts/date-range.js
+++ public/scripts/date-range.js
@@ -1,13 +1,13 @@
 $(function() {
   $('input[name="datefilter"]').daterangepicker({
     // minDate: Date.now(),
-    // minDate: Date.now(),
+    minDate: Date.now(),
     "minDate": today,
 
-    // autoUpdateInput: false,
-    // locale: {
-    //   cancelLabel: 'Clear'
-    // }
+    autoUpdateInput: false,
+    locale: {
+      cancelLabel: 'Clear'
+    }
   });
   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
