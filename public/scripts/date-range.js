755cfb78068b1defc6e7b869efaefc04fd16c138
Dianne <mdianne.gabriel@gmail.com>
2021-08-22 17:28:26 -0400

    Experiment new daterangepicker

--- public/scripts/date-range.js
+++ public/scripts/date-range.js
@@ -1,12 +1,21 @@
 $(function() {
   $('input[name="datefilter"]').daterangepicker({
-    autoUpdateInput: false,
-    locale: {
-      cancelLabel: 'Clear'
-    },
-    "minDate": "-1d",
+    // autoUpdateInput: false,
+    // locale: {
+    //   cancelLabel: 'Clear'
+    // },
+    // "minDate": "-1d",
     // minDate: today,
     // minDate: Date.now(),
+    "autoApply": true,
+    "autoUpdateInput": false,
+    "singleDatePicker": true,
+    "minDate": today,
+    "maxDate": maxLimitDate,
+    "opens": "left",
+    "locale": {
+      format: 'DD MMM YYYY'
+    }
   });
   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
