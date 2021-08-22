2aa2bf1e40f7d81a0721bdbc0ea230774aed7a20
Dianne <mdianne.gabriel@gmail.com>
2021-08-22 17:39:49 -0400

    Change attribute

--- public/scripts/date-range.js
+++ public/scripts/date-range.js
@@ -24,10 +24,10 @@ $(function() {
       multidateSeparator: ",",
       toggleActive: true
   });
-  $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+  $('.datefilter').on('apply.daterangepicker', function(ev, picker) {
     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
   });
-  $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+  $('.datefilter').on('cancel.daterangepicker', function(ev, picker) {
     $(this).val('');
   });
 });
