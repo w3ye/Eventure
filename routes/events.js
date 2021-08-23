328a0a054331c137351c6ea6e8045dc4fdafb7de
William Ye <w3ye@pm.me>
2021-08-23 10:36:57 -0400

    implement createEvent not tested yet

--- routes/events.js
+++ routes/events.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  const e = new EVENT(db);
-  router.get("/create", (req, res) => {
-    e.events
+  const event = new EVENT(db);
+  router.post("/create", (req, res) => {
+    const { ownerId, startDate, endDate, closeSubmission, detail } = req.body;
+    event.newEvent(ownerId, startDate, endDate, closeSubmission, detail)
       .then(result => {
-        console.log(result);
+        return result;
       })
       .catch(err => {
-        console.log(err);
+        res.status(500).send('Failed to create');
       });
   });
   return router;
