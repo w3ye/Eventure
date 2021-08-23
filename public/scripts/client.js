b372a8d7ae7dfdef42c7332877b74c0dc0a12271
Dianne <mdianne.gabriel@gmail.com>
2021-08-23 09:45:57 -0400

    Modify modal logic. Close and submit button now works using js functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,6 @@ const escape = function(str) {
   return div.innerHTML;
 };
 
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-
-// const renderTimeslots = function() {
-//   $('#add-timeslots').append(addTimeslots());
-// }
-
-// const addTimeslots = function() {
-//   const $timeslot = $('<section>').addClass('add-timeslots');
-//   const html = `
-//     <div class="modal" id="modal">
-//       <div class="modal-header">
-//         <p class="fix-title">Eventure Title</p>
-//         <button data-close-button class="close-button">&times;</button>
-//       </div>
-//       <div class="modal-body">
-//         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-//         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-//         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-//         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-//         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-//         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-//         mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-//         adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-//         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-//         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-//         in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-//         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-//         qui officia deserunt mollit anim id est laborum.
-//       </div>
-//       <div class="submit">
-//         <button type="submit" form="nameform" value="Submit" class="button-submit">Submit</button>
-//       </div>
-//     </div>
-//   `;
-//   return $timeslot.html(html);
-// }
-
 const enterTitle = function() {
   $('.title-create').on('click', () => {
     $('.master-body').slideDown();
@@ -52,12 +15,4 @@ const enterTitle = function() {
 $(document).ready(function() {
   $('.master-body').hide();
   enterTitle();
-
-  $('.button-next').on('click', () => {
-    $('.catchphrase').hide();
-    renderTimeslots();
-    $('#overlay').css('opacity', '1');
-  });
-
-
 });
