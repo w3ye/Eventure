b88e6f42c52ee4c92313443d1a6460bf58a56394
Dianne <mdianne.gabriel@gmail.com>
2021-08-23 13:02:01 -0400

    Implement timepicker

--- public/scripts/timepicker.js
+++ public/scripts/timepicker.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
-  $('input.timepicker').timepicker({
-    timeFormat: 'h:mm p',
-    interval: 60,
-    minTime: '10',
-    maxTime: '6:00pm',
-    defaultTime: '11',
-    startTime: '10:00',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true
+  $('#start').timepicker({
+    timeFormat : 'hh:mm a',
+    interval : 15,
+    maxTime : '11:45 PM',
+    startTime : '12:00 AM',
+    endTime : '11:45 PM',
+    dynamic : false,
+    dropdown : true,
+    scrollbar : true
   });
+
+  $('#start')
+  .timepicker('option', 'change', function(time) {
+    const startHour = parseInt($('#start').val().substring(0, 2));
+    const startMinutes = $('#start').val().substring(2, 8);
+    const endMinTime = (startHour).toString().concat(startMinutes + 0.15);
+
+    $('#end').timepicker('option', 'minTime', endMinTime);
+    $('#end').timepicker('setTime', endMinTime);
+  });
+
+  $('#end').timepicker({
+    timeFormat : 'hh:mm a',
+    interval : 15,
+    maxTime : '11:45 PM',
+    endTime : '11:45 PM',
+    dynamic : false,
+    dropdown : true,
+    scrollbar : true
+  });
+
 });
