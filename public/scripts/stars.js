27af3830cd8ff29ec5c5b5b9dcd2c1c5cd2977e5
Dianne <mdianne.gabriel@gmail.com>
2021-08-24 08:46:07 -0400

    Move some divs to a js file for modularity

---
+++ public/scripts/stars.js
@@ -0,0 +1,15 @@
+$(() => {
+  const $starsContainer = $('#stars');
+  const $stars =`
+    <div class="star2"></div>
+    <div class="star4"></div>
+    <div class="star5"></div>
+    <div class="star6"></div>
+    <div class="star7"></div>
+    <div class="star8"></div>
+    <div class="star9"></div>
+    <div class="star10"></div>
+    <div class="star11"></div>
+  `;
+  return $starsContainer.html($stars)
+});
