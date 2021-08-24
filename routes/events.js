0ae9241f9c1144ec67295f2308d41f3b6f01ea9a
William Ye <w3ye@pm.me>
2021-08-24 16:35:00 -0400

    addes event to the created user

--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,7 @@ const EVENT = require("../lib/event-queries");
 const USER = require("../lib/user-queries");
 const express = require("express");
 const router = express.Router();
-const { splitName } = require('../helper');
+const { splitName } = require("../helper");
 
 module.exports = (db) => {
   const event = new EVENT(db);
@@ -12,27 +12,30 @@ module.exports = (db) => {
     const name = splitName(req.body.name);
     const userQueryObj = {
       firstName: name[0],
-      lastName: (name.length === 2) ? name[1] : null,
-      email: req.body.email
+      lastName: name.length === 2 ? name[1] : null,
+      email: req.body.email,
     };
 
-    user.newUser(userQueryObj).then(() => {
-      res.json({ success: true });
-    });
-    
+    user
+      .newUser(userQueryObj)
+      .then((result) => {
+        userQueryObj["id"] = result.id;
+      })
+      .then(() => {
+        const eventQueryObj = {
+          ownerId: userQueryObj["id"],
+          startDate: req.body.from,
+          endDate: req.body.to,
+          closeSubmission: req.body.deadline,
+          title: req.body.title,
+          detail: req.body.description,
+          link: null,
+        };
+        event.newEvent(eventQueryObj).then(() => {
+          res.json({ success: true});
+        });
+      });
 
-    const eventQueryObj = {
-      ownerId: 1,
-      startDate: req.body.from,
-      endDate: req.body.to,
-      closeSubmission: req.body.deadline,
-      title: req.body.title,
-      detail: req.body.description,
-      link: null,
-    };
-    // event.newEvent(eventQueryObj).then(() => {
-    //   res.json({ success: true });
-    // });
   });
 
   return router;
