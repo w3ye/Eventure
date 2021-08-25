016ac47b1220f06cf5d39fd349f1093f79768ddc
Dianne <mdianne.gabriel@gmail.com>
2021-08-24 21:39:29 -0400

    Fix conflict

--- public/scripts/create-event.js
+++ public/scripts/create-event.js
@@ -1,36 +1,18 @@
-const eventCreation = function () {
-  const $eventContainer = $("#create-event");
+const eventCreation = function() {
+  const $eventContainer = $('#create-event');
 
   const $creation = `
-  <div class="catchphrase">
-    <p class="catchphrase-words">Start your&nbsp;</p>
-    <p class="catchphrase-eventure">
-    Eventure
-  </p>
+  <div class="master-catchphrase">
+    <p class="master-catchphrase-words">Start your&nbsp;</p>
+    <p class="master-catchphrase-eventure">Eventure</p>
   </div>
-
   <div class="master-box" id="master-box">
     <form class="title" id="new-event">
       <div class="master-header">
-          <label for="title">
-            <input type="text" name="title" class="title-create" placeholder="Eventure Title" maxlength="30" autocomplete="off" required>
-          </label>
+        <label for="title">
+          <input type="text" name="title" class="title-create" placeholder="Eventure Title" maxlength="30" autocomplete="off" required>
+        </label>
       </div>
-
-    <div class="master-body">
-      <p class="details">Select Date(s):</p>
-      <div class="indent date">
-        <label for="from">From</label>
-        <input type="text" id="from" name="from" required>
-        <label for="to">To</label>
-        <input type="text" id="to" name="to" required>
-      </div>
-      <p class="details">Submission Deadline:</p>
-      <div class="indent date">
-        <label for="deadline"><i>(Optional)</i></label>
-        <input name="deadline" type="text" id="deadline">
-      </div>
-
       <div class="master-body">
         <p class="details">Select Date(s):</p>
         <div class="indent date">
@@ -50,7 +32,6 @@ const eventCreation = function () {
         <input name="name" id="owner-name" class="owner" maxlength="30" required></textarea>
         <p class="details">E-mail:</p>
         <input name="email" id="owner-email" class="owner" maxlength="50" required></textarea>
-
         <div class="next">
           <button type="submit" id="next-button" onclick="openForm()">Next</button>
         </div>
@@ -62,14 +43,11 @@ const eventCreation = function () {
   return $eventContainer.html($creation);
 };
 
-const enterTitle = function () {
-  $(".title-create").on("click", () => {
-    $(".master-body").slideDown();
-    $(".master-body").show();
-    $(".master-header").css(
-      "box-shadow",
-      "0 50px 200px -200px rgba(0,0,0,0.5), 0 10px 10px -10px rgba(0,0,0,0.3)"
-    );
+const enterTitle = function() {
+  $('.title-create').on('click', () => {
+    $('.master-body').slideDown();
+    $('.master-body').show();
+    $('.master-header').css('box-shadow', '0 50px 200px -200px rgba(0,0,0,0.5), 0 10px 10px -10px rgba(0,0,0,0.3)');
   });
 };
 
@@ -77,13 +55,13 @@ const nextButton = function() {
   $("#next-button").click((event) => {
     event.preventDefault();
     const serialize = $("#new-event").serialize();
-    $.post("/create", serialize).done(console.log("success"));
+    console.log(serialize);
   });
 };
 
 $(document).ready(function() {
   eventCreation();
-  $(".master-body").hide();
+  $('.master-body').hide();
   enterTitle();
   nextButton();
 });
